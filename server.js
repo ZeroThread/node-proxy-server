143d454def47d67de6f08d01ef30f284bec211aa
Divinewill ZERO <zerothread@users.noreply.github.com>
2022-01-23 18:07:35 +0100

    [ Chore ⚙ ] Implemented a simple Node.JS proxy server

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const app = require('./src/app');
 
 const PORT = process.env.PORT;
+const HOST = process.env.HOST;
 app.listen(PORT, () => {
-  console.log(`App 🚀 is running on port ${PORT}`);
+  console.log(`App 🚀 is running on port ${HOST}:${PORT}`);
 });
--- src/app.js
+++ src/app.js
@@ -3,5 +3,35 @@ const morgan = require('morgan');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
+app.use(morgan('dev'));
+
+const API_SERVICE_URL = process.env.API_SERVICE_URL;
+
+app.get('/info', (req, res, next) => {
+  res.send(
+    `This is a proxy service which proxies to Billing account and Account API's`
+  );
+});
+
+// Authorization
+app.use('', (req, res, next) => {
+  if (req.headers.authorization) {
+    next();
+  } else {
+    res.sendStatus(403);
+  }
+});
+
+// Proxy endpoints
+app.use(
+  '/json_placeholder',
+  createProxyMiddleware({
+    target: API_SERVICE_URL,
+    changeOrigin: true,
+    pathRewrite: {
+      [`^/json_placeholder`]: '',
+    },
+  })
+);
 
 module.exports = app;
